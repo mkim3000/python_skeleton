9469e17c5bac3592816b9b07b9f5c2b0669173ed
Michael Kim <mkim3000@users.noreply.github.com>
2016-07-05 12:56:11 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,5 +5,17 @@ except ImportError:
 
 config = {
     'description': 'M Project',
+    'author': 'My Name',
+    'url': 'URL to get it at.',
+    'download_url': 'Where to download it.',
+    'author_email': 'My email.',
+    'version': '0.1',
+    'install_requires': ['nose'],
+    'packages': ['NAME'],
+    'scripts': [],
+    'name': 'projectname'
+}
+
+setup(**config)
 
 
