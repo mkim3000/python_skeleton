df93964155b288843240bd132c8d9f3235cc519d
Michael Kim <mkim3000@users.noreply.github.com>
2016-07-05 17:21:54 -0700

    Update and rename bin/my.py to example.py

---
+++ example.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+“””Retrieve and print words from a URL.
+
+Usage:
+
+        python3 words.py <URL>
+“””
+
+import sys
+from urllib.request import urlopen
+
+
+def fetch_words(url):
+    “””Fetch a list of words from a URL.
+
+    Args:
+        url: The URL of a UTF-8 text document.
+
+    Returns:
+        A list of strings containing the words from the document.
+    “””
+    with urlopen('http://sixty-north.com/c/t.txt') as story:
+        story_words = []
+        for line in story:
+            line_words = line.decode('utf-8').split()
+            for word in line_words:
+                story_words.append(word)
+    return story_words
+
+
+def print_items(items):
+    for item in items:
+        print(item)
+
+
+def main(url):
+    words = fetch_words(url)
+    print_items(words) # code comment can be done this way with sharp
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
