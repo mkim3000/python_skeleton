f554386053f3a55258da94415b10a09de6c1ce92
Michael Kim <mkim3000@users.noreply.github.com>
2016-07-10 02:50:28 -0700

    Update my_example.py

--- bin/my_example.py
+++ bin/my_example.py
@@ -38,10 +38,33 @@ def print_items(items):
     for item in items:
         print(item)
 
+#http://www.diveintopython.net/scripts_and_streams/command_line_arguments.html
+def main(argv):                          1
+    grammar = "kant.xml"                
+    try:                                
+        opts, args = getopt.getopt(argv, "hg:d", ["help", "grammar="])
+    except getopt.GetoptError:          
+        usage()                         
+        sys.exit(2)                     
+    for opt, arg in opts:                2
+        if opt in ("-h", "--help"):      3
+            usage()                     
+            sys.exit()                  
+        elif opt == '-d':                4
+            global _debug               
+            _debug = 1                  
+        elif opt in ("-g", "--grammar"): 5
+            grammar = arg               
 
-def main(url):
-    words = fetch_words(url)
-    print_items(words) # code comment can be done this way with sharp
+    source = "".join(args)               6
+
+    k = KantGenerator(grammar, source)
+    print k.output()
+
+
+#def main(url):
+#    words = fetch_words(url)
+#    print_items(words) # code comment can be done this way with sharp
 
 
 if __name__ == '__main__':
